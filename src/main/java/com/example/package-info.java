1c08239527abe9319decdb793f9bd689b2686dec
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-08-07 23:15:42 +0300

    Delete package

--- src/main/java/com/example/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains utility classes for handling common operations.
- * Some classes provide helper methods for string manipulations,
- * date formatting, and file handling.
- */
-package com.example;
