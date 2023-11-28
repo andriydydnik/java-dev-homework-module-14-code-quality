519858744a3db95e165ebb10bed02f39ffdb50e4
Artuar <artur.dikun@gmail.com>
2023-11-28 19:42:57 +0200

    hw3

---
+++ src/main/java/game/LetsPlay.java
@@ -0,0 +1,8 @@
+package game;
+
+public class LetsPlay {
+    public static void main(String[] args) {
+        App play = new App();
+        play.startGame();
+    }
+}
---
+++ src/main/java/game/Win.java
@@ -0,0 +1,9 @@
+package game;
+
+public class Win {
+    public static boolean result(char[] box, char value) {
+        return ((box[0] == value && box[1] == value && box[2] == value) || (box[3] == value && box[4] == value && box[5] == value) || (box[6] == value && box[7] == value && box[8] == value) ||
+                (box[0] == value && box[3] == value && box[6] == value) || (box[1] == value && box[4] == value && box[7] == value) || (box[2] == value && box[5] == value && box[8] == value) ||
+                (box[0] == value && box[4] == value && box[8] == value) || (box[2] == value && box[4] == value && box[6] == value));
+    }
+}
